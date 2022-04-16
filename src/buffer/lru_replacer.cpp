5134ed57cc73b99ec1adff096722db0632c4aea4
BilyZ98 <zhuangzhutao@gmail.com>
2022-04-16 20:39:09 +0800

    update lru replacer include files

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/lru_replacer.h"
+#include <assert.h>
 #include <algorithm>
 #include <mutex>
 namespace bustub {
